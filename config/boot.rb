f930df79035daeab896e5220d10ce7f1bab9a45a
Ian Young <ian.greenleaf@gmail.com>
2012-02-15 23:56:42 -0800

    Require DataMapper migrations
    Fixes a method-not-found error

--- config/boot.rb
+++ config/boot.rb
@@ -9,5 +9,6 @@ Dir["./lib/**/*.rb"].each { |f| require f }
 
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
+require  'dm-migrations'
 DataMapper.auto_upgrade!
 

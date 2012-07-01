83555ff63e5ae3d6d95ac96a03019cbd162e292e
Andy Waite <github.aw@andywaite.com>
2012-07-01 20:21:11 +0100

    Fail nicer when DATABASE_URL isn't set

--- config/boot.rb
+++ config/boot.rb
@@ -7,6 +7,8 @@ Bundler.require(:default, ENV["RACK_ENV"].to_sym)
 
 Dir["./lib/**/*.rb"].each { |f| require f }
 
+raise 'DATABASE_URL not set (see README)' unless ENV['DATABASE_URL']
+
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
 DataMapper.auto_upgrade!
